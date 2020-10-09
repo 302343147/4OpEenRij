09761815e661d77608fdbcc0995ff9d63fe9fcf5
Eryk <302343147@student.rocmondriaan.nl>
2020-10-09 20:39:37 +0200

    Er wordt een alert gegeven van welk kleur gewonnen heeft. (+ je naam als je die ingevuld hebt)

--- 4OpEenRijGameMetMVC/js/Model.js
+++ 4OpEenRijGameMetMVC/js/Model.js
@@ -46,54 +46,63 @@ class Model
 
     playerWin(){ //Dit kon slimmer, maar ik had alleen één idee over hoe ik de winsysteem zou kunnen maken.
         let name1 = document.getElementById("player1name").innerText;
+        name1 = name1.replace("Points:", "");
+        name1 = name1.replace("Player", "");
+        name1 = name1.replace(/[0-9]/g, "");
+
         let name2 = document.getElementById("player2name").innerText;
+        name2 = name2.replace("Points:", "");
+        name2 = name2.replace("Player", "");
+        name2 = name2.replace(/[0-9]/g, "");
+
         let points1 = Number(document.getElementById("points1").innerText);
         let points2 = Number(document.getElementById("points2").innerText);
+
         for (let rij = 5; rij >= 0; rij--){
             if(this.vakjes[rij][0] === "rood" && this.vakjes[rij][1] === "rood" && this.vakjes[rij][2] === "rood" && this.vakjes[rij][3] === "rood"){
-                alert(name1 + " wint!");
+                alert(name1 + " rood wint!");
                 points1++;
                 document.getElementById("points1").innerText = points1;
                 this.leegVakjes();
             }
             if(this.vakjes[rij][1] === "rood" && this.vakjes[rij][2] === "rood" && this.vakjes[rij][3] === "rood" && this.vakjes[rij][4] === "rood"){
-                alert(name1 + " wint!");
+                alert(name1 + " rood wint!");
                 points1++;
                 document.getElementById("points1").innerText = points1;
                 this.leegVakjes();
             }
             if(this.vakjes[rij][2] === "rood" && this.vakjes[rij][3] === "rood" && this.vakjes[rij][4] === "rood" && this.vakjes[rij][5] === "rood"){
-                alert(name1 + " wint!");
+                alert(name1 + " rood wint!");
                 points1++;
                 document.getElementById("points1").innerText = points1;
                 this.leegVakjes();
             }
             if(this.vakjes[rij][3] === "rood" && this.vakjes[rij][4] === "rood" && this.vakjes[rij][5] === "rood" && this.vakjes[rij][6] === "rood"){
-                alert(name1 + " wint!");
+                alert(name1 + " rood wint!");
                 points1++;
                 document.getElementById("points1").innerText = points1;
                 this.leegVakjes();
             }
             if(this.vakjes[rij][0] === "geel" && this.vakjes[rij][1] === "geel" && this.vakjes[rij][2] === "geel" && this.vakjes[rij][3] === "geel"){
-                alert(name2 + " wint!");
+                alert(name2 + " geel wint!");
                 points2++;
                 document.getElementById("points2").innerText = points2;
                 this.leegVakjes();
             }
             if(this.vakjes[rij][1] === "geel" && this.vakjes[rij][2] === "geel" && this.vakjes[rij][3] === "geel" && this.vakjes[rij][4] === "geel"){
-                alert(name2 + " wint!");
+                alert(name2 + " geel wint!");
                 points2++;
                 document.getElementById("points2").innerText = points2;
                 this.leegVakjes();
             }
             if(this.vakjes[rij][2] === "geel" && this.vakjes[rij][3] === "geel" && this.vakjes[rij][4] === "geel" && this.vakjes[rij][5] === "geel"){
-                alert(name2 + " wint!");
+                alert(name2 + " geel wint!");
                 points2++;
                 document.getElementById("points2").innerText = points2;
                 this.leegVakjes();
             }
             if(this.vakjes[rij][3] === "geel" && this.vakjes[rij][4] === "geel" && this.vakjes[rij][5] === "geel" && this.vakjes[rij][6] === "geel"){
-                alert(name2 + " wint!");
+                alert(name2 + " geel wint!");
                 points2++;
                 document.getElementById("points2").innerText = points2;
                 this.leegVakjes();
@@ -101,37 +110,37 @@ class Model
         }
         for(let kolom = 0; kolom <= 6; kolom++){
             if(this.vakjes[5][kolom] === "rood" && this.vakjes[4][kolom] === "rood" && this.vakjes[3][kolom] === "rood" && this.vakjes[2][kolom] === "rood"){
-                alert(name1 + " wint!");
+                alert(name1 + " rood wint!");
                 points1++;
                 document.getElementById("points1").innerText = points1;
                 this.leegVakjes();
             }
             if(this.vakjes[4][kolom] === "rood" && this.vakjes[3][kolom] === "rood" && this.vakjes[2][kolom] === "rood" && this.vakjes[1][kolom] === "rood"){
-                alert(name1 + " wint!");
+                alert(name1 + " rood wint!");
                 points1++;
                 document.getElementById("points1").innerText = points1;
                 this.leegVakjes();
             }
             if(this.vakjes[3][kolom] === "rood" && this.vakjes[2][kolom] === "rood" && this.vakjes[1][kolom] === "rood" && this.vakjes[0][kolom] === "rood"){
-                alert(name1 + " wint!");
+                alert(name1 + " rood wint!");
                 points1++;
                 document.getElementById("points1").innerText = points1;
                 this.leegVakjes();
             }
             if(this.vakjes[5][kolom] === "geel" && this.vakjes[4][kolom] === "geel" && this.vakjes[3][kolom] === "geel" && this.vakjes[2][kolom] === "geel"){
-                alert(name2 + " wint!");
+                alert(name2 + " geel wint!");
                 points2++;
                 document.getElementById("points2").innerText = points2;
                 this.leegVakjes();
             }
             if(this.vakjes[4][kolom] === "geel" && this.vakjes[3][kolom] === "geel" && this.vakjes[2][kolom] === "geel" && this.vakjes[1][kolom] === "geel"){
-                alert(name2 + " wint!");
+                alert(name2 + " geel wint!");
                 points2++;
                 document.getElementById("points2").innerText = points2;
                 this.leegVakjes();
             }
             if(this.vakjes[3][kolom] === "geel" && this.vakjes[2][kolom] === "geel" && this.vakjes[1][kolom] === "geel" && this.vakjes[0][kolom] === "geel"){
-                alert(name2 + " wint!");
+                alert(name2 + " geel wint!");
                 points2++;
                 document.getElementById("points2").innerText = points2;
                 this.leegVakjes();
